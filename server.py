a45b0af4e55b00ad70567f4bd2e8af63909d17a1
buildsbyneha <infonehak.work@gmail.com>
2025-10-06 17:22:24 +0530

    hiiiiii

--- server.py
+++ server.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, render_template, redirect, url_for
 from weather import get_current_weather
 from waitress import serve
-import os
 
 app = Flask(__name__)
 
@@ -37,5 +36,4 @@ def get_weather():
       
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    serve(app, host="0.0.0.0", port=8000)
